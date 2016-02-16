83a901cbf4dcadf889944b3db4f04c845df048a0
llaull <laul@llovem.eu>
2016-02-16 17:00:34 +0100

    Signed-off-by: llaull <laul@llovem.eu>

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,12 +2,12 @@
 // Tests/bootstrap.php
 $loader = @include __DIR__ . '/../vendor/.composer/autoload.php';
 if (!$loader) {
-    die(<<<'EOT'
-You must set up the project dependencies, run the following commands:
-wget http://getcomposer.org/composer.phar
-php composer.phar install
-EOT
-    );
+//    die(<<<'EOT'
+//You must set up the project dependencies, run the following commands:
+//wget http://getcomposer.org/composer.phar
+//php composer.phar install
+//EOT
+//    );
 }
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
