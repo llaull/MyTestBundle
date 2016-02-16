480068f37df15c9c6c395ab01cdd23c9b0f8e291
llaull <laul@llovem.eu>
2016-02-16 16:29:01 +0100

    Signed-off-by: llaull <laul@llovem.eu>

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,23 @@
 <?php
-$file = __DIR__.'/../vendor/autoload.php';
-if (!file_exists($file)) {
-    throw new RuntimeException('Install dependencies using composer to run the test suite.');
+// Tests/bootstrap.php
+$loader = @include __DIR__ . '/../vendor/.composer/autoload.php';
+if (!$loader) {
+    die(<<<'EOT'
+You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+EOT
+    );
 }
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
-$autoload = require_once $file;
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'laul\\MyTestBundle\\')) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
+    }
+});
